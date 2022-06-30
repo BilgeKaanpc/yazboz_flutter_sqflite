4a4e22cd46a89424b17ec6efd61b9e473261ca80
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-06-30 11:53:09 +0300

    database eklendi

---
+++ lib/databaseHelpers/databaseHelper.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper{
+
+  static final String databaseName = "yazboz.sqlite";
+
+
+  static Future<Database> dbErisim() async{
+    String databasePath = join(await getDatabasesPath(),databaseName);
+    if(await databaseExists(databasePath)){
+    }else{
+
+      ByteData data = await rootBundle.load("database/$databaseName");
+      List<int> bytes = data.buffer.asInt8List(data.offsetInBytes,data.lengthInBytes);
+      await File(databasePath).writeAsBytes(bytes,flush: true);
+
+    }
+    return openDatabase(databasePath);
+  }
+
+
+
+
+
+
+}
