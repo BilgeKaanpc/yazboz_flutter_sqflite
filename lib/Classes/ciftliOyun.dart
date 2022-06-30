338bc144ae3f512ef5846297cb03607c4c79b4d7
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-06-30 13:05:04 +0300

    çitli oyunlar sınıfı eklendi

---
+++ lib/Classes/ciftliOyun.dart
@@ -0,0 +1,25 @@
+class CiftliOyun{
+  late int Id;
+  late String teamA;
+  late String teamB;
+  late String teamApuan;
+  late String teamBpuan;
+  late String teamAceza;
+  late String teamBceza;
+  late int toplamA;
+  late int toplamB;
+  late String tarih;
+
+  CiftliOyun(
+      this.Id,
+      this.teamA,
+      this.teamB,
+      this.teamApuan,
+      this.teamBpuan,
+      this.teamAceza,
+      this.teamBceza,
+      this.toplamA,
+      this.toplamB,
+      this.tarih
+      );
+}
