6a5169651562e37644f5f6501ace61fc9d814883
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-06-30 12:29:04 +0300

    Ana ekran tasarımı güncellendi

---
+++ lib/Design/DesignVeriables.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class DesignVeribles{
+    static const mainColor = Colors.indigo;
+    static final TextStyle baslikStyle = GoogleFonts.concertOne(
+        fontSize: 24,fontWeight: FontWeight.w900, color: mainColor
+    );
+
+    static final padding4 = EdgeInsets.all(4.0);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yazboz/Design/DesignVeriables.dart';
 
 import 'databaseHelpers/databaseHelper.dart';
 
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: DesignVeribles.mainColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -25,8 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
@@ -34,30 +34,85 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  getir()async{
-    print("gidrik");
+  getir() async {
     var db = await DatabaseHelper.dbErisim();
-    print("çıktık");
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+            child: Text(
+          "Yaz-Boz",
+          style: DesignVeribles.baslikStyle,
+        )),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             Text(
-              'asdas',
+            Expanded(
+              flex: 1,
+              child: Text("Çiftli Oyunlar"),
             ),
+            Expanded(
+              flex: 3,
+              child: Container(),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text("Çiftli Oyunlar"),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(),
+            ),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: DesignVeribles.padding4,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Buton1"),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: DesignVeribles.padding4,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Buton2"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Buton3"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Buton4"),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getir,
       ),
     );
   }
